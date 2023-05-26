e9eaa9c2a591c852f2773311d2cdfea340ef8fbf
Hedi Rivas <hedi.rivas@gmail.com>
2023-05-26 16:47:07 +0200

    routes signup et signin done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+require('./models/connectionString');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/users');
 
-/* GET users listing. */
-router.get('/login', function (req, res, next) {
-  res.send('Hello la team');
+const { checkBody } = require('../modules/checkBody');
+
+router.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields!' });
+    return;
+  }
+
+  User.findOne({ username: username }).then((data) => {
+    if (data === null) {
+      const newUser = new User({
+        username: username,
+        password: password,
+      });
+      newUser.save().then((data) => {
+        res.json({ result: true });
+      });
+    } else {
+      res.json({ result: false, error: 'User already exists !' });
+    }
+  });
+});
+
+router.post('/signin', (req, res) => {
+  const { username } = req.body;
+
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields!' });
+    return;
+  }
+
+  User.findOne({ username: username }).then((data) => {
+    if (data) {
+      res.json({ result: true });
+    } else {
+      res.json({ result: false, error: 'User not found!' });
+    }
+  });
 });
 
 module.exports = router;

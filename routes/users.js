49fce78306638b3e26baec89bdc9b0b106c0c1e1
Hedi Rivas <hedi.rivas@gmail.com>
2023-05-29 16:26:02 +0200

    envoie du token vers le fornt + sécuristation du mdp

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/users');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
 const { checkBody } = require('../modules/checkBody');
 
@@ -11,15 +13,19 @@ router.post('/signup', (req, res) => {
     res.json({ result: false, error: 'Missing or empty fields!' });
     return;
   }
+  const token = uid2(32);
+  const hash = bcrypt.hashSync(password, 10);
 
   User.findOne({ username: username }).then((data) => {
     if (data === null) {
       const newUser = new User({
         username: username,
-        password: password,
+        password: hash,
+        token: token,
+        canBookmark: true,
       });
       newUser.save().then((data) => {
-        res.json({ result: true });
+        res.json({ result: true, token: token });
       });
     } else {
       res.json({ result: false, error: 'User already exists !' });
@@ -28,7 +34,7 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/signin', (req, res) => {
-  const { username } = req.body;
+  const { username, password } = req.body;
 
   if (!checkBody(req.body, ['username', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields!' });
@@ -36,10 +42,22 @@ router.post('/signin', (req, res) => {
   }
 
   User.findOne({ username: username }).then((data) => {
-    if (data) {
-      res.json({ result: true });
+    if (bcrypt.compareSync(password, data.password)) {
+      res.json({ result: true, token: data.token });
     } else {
       res.json({ result: false, error: 'User not found!' });
+    }
+  });
+});
+
+router.get('/canBookmark/:token', (req, res) => {
+  const { token } = req.params;
+
+  User.findOne({ token: token }).then((data) => {
+    if (data) {
+      res.json({ result: true, canBookmark: data.canBookmark });
+    } else {
+      res.json({ result: false });
     }
   });
 });
